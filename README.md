6e5d9d751e67a8310a07413f1d87f72fe3024610
Dorian Marié <dorian@dorianmarie.fr>
2021-10-29 22:58:32 +0200

    fix a lot of css by replacing variables with default values

--- README.md
+++ README.md
@@ -37,3 +37,63 @@ cat links.txt | grep "tailwindcss.com/docs/" | each "puts l.split('#').first" | 
 ```
 
 Then `bundle && ./scrapper && prettier --write .`
+
+### Unsupported (yet):
+
+Auto-generated. Mostly because of CSS variables, and random docs pages, and because I don't use them, again, pull requests welcome ❤️
+
+- container
+- adding-base-styles
+- adding-new-utilities
+- backdrop-blur
+- backdrop-brightness
+- backdrop-contrast
+- backdrop-grayscale
+- backdrop-hue-rotate
+- backdrop-invert
+- backdrop-opacity
+- backdrop-saturate
+- backdrop-sepia
+- background-opacity
+- blur
+- border-opacity
+- breakpoints
+- brightness
+- browser-support
+- configuration
+- configuring-variants
+- contrast
+- customizing-colors
+- customizing-spacing
+- dark-mode
+- divide-opacity
+- drop-shadow
+- editor-support
+- extracting-components
+- functions-and-directives
+- gradient-color-stops
+- grayscale
+- hover-focus-and-other-states
+- hue-rotate
+- installation
+- invert
+- just-in-time-mode
+- optimizing-for-production
+- placeholder-opacity
+- plugins
+- preflight
+- presets
+- responsive-design
+- ring-color
+- ring-opacity
+- rotate
+- saturate
+- scale
+- sepia
+- skew
+- text-opacity
+- theme
+- translate
+- upgrading-to-v2
+- using-with-preprocessors
+- utility-first
--- scrapper.rb
+++ scrapper.rb
@@ -29,12 +29,59 @@ def tr_to_css(tr)
   clazz, value = tr.css("td").map(&:text)
   clazz = clazz.gsub(".", "\\.").gsub("/", "\\/")
   value = value.lines.grep_v(/^--/).join
-  value = value.gsub(/var\([a-z0-9, #-]+\)/, "1")
+  value.gsub!("var(--tw-backdrop-blur)", "blur(0)")
+  value.gsub!("var(--tw-backdrop-brightness)", "brightness(1)")
+  value.gsub!("var(--tw-backdrop-contrast)", "contrast(1)")
+  value.gsub!("var(--tw-backdrop-grayscale)", "grascale(0)")
+  value.gsub!("var(--tw-backdrop-hue-rotate)", "hue-rotate(0deg)")
+  value.gsub!("var(--tw-backdrop-invert)", "invert(0)")
+  value.gsub!("var(--tw-backdrop-opacity)", "1")
+  value.gsub!("var(--tw-backdrop-saturate)", "saturate(1)")
+  value.gsub!("var(--tw-backdrop-sepia)", "sepia(0)")
+  value.gsub!("var(--tw-bg-opacity)", "1")
+  value.gsub!("var(--tw-blur)", "blur(0)")
+  value.gsub!("var(--tw-border-opacity)", "1")
+  value.gsub!("var(--tw-brightness)", "brightness(1)")
+  value.gsub!("var(--tw-contrast)", "contrast(1)")
+  value.gsub!("var(--tw-divide-opacity)", "1")
+  value.gsub!("var(--tw-divide-x-reverse)", "0")
+  value.gsub!("var(--tw-divide-y-reverse)", "0")
+  value.gsub!("var(--tw-drop-shadow)", "drop-shadow(0 0 #0000)")
+  value.gsub!("var(--tw-gradient-stops)", "#f9fafb, rgba(249, 250, 251, 0)")
+  value.gsub!("var(--tw-grayscale)", "grayscale(0)")
+  value.gsub!("var(--tw-hue-rotate)", "hue-rotate(0deg)")
+  value.gsub!("var(--tw-invert)", "invert(0)")
+  value.gsub!("var(--tw-placeholder-opacity)", "1")
+  value.gsub!("var(--tw-ring-color)", "rgba(59, 130, 246, 0.5)")
+  value.gsub!("var(--tw-ring-inset)", "")
+  value.gsub!("var(--tw-ring-offset-color)", "#fff")
+  value.gsub!("var(--tw-ring-offset-shadow)", "0 0 #0000")
+  value.gsub!("var(--tw-ring-offset-width)", "0")
+  value.gsub!("var(--tw-ring-shadow)", "0 0 #0000")
+  value.gsub!("var(--tw-rotate)", "0")
+  value.gsub!("var(--tw-saturate)", "saturate(1)")
+  value.gsub!("var(--tw-scale-x)", "0")
+  value.gsub!("var(--tw-scale-y)", "0")
+  value.gsub!("var(--tw-sepia)", "sepia(0)")
+  value.gsub!("var(--tw-shadow)", "0 0 #0000")
+  value.gsub!("var(--tw-skew-x)", "0")
+  value.gsub!("var(--tw-skew-y)", "0")
+  value.gsub!("var(--tw-space-x-reverse)", "0")
+  value.gsub!("var(--tw-space-y-reverse)", "0")
+  value.gsub!("var(--tw-text-opacity)", "1")
+  value.gsub!("var(--tw-translate-x)", "0")
+  value.gsub!("var(--tw-translate-y)", "0")
+  value.gsub!("var(--tw-ring-offset-shadow, 0 0 #0000)", "0 0 #0000")
+  value.gsub!("var(--tw-ring-shadow, 0 0 #0000)", "0 0 #0000")
+
   return if value.blank?
   ".#{clazz} { #{value} }"
 end
 
 slugs = []
+
+puts "Unsupported (yet):"
+puts "- container"
 
 LINKS.each do |link|
   slug = link.split("/").last
@@ -42,9 +89,12 @@ LINKS.each do |link|
   trs = doc.css("tbody.align-baseline tr")
 
   content = trs.map { |tr| tr_to_css(tr) }.compact.join("\n\n")
-  next if content.blank?
-  slugs << slug
-  File.write("#{ASSETS_PATH}/#{slug}.css", content)
+  if content.present?
+    slugs << slug
+    File.write("#{ASSETS_PATH}/#{slug}.css", content)
+  else
+    puts "- #{slug}"
+  end
 end
 
 File.write(
